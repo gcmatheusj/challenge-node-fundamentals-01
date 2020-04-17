60a9977bb57155e55b43850699a67c12b09df90f
Matheus <goca1377@gmail.com>
2020-04-17 07:19:27 -0300

    ending challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (accumulator: number, current: Transaction) => {
+        if (current.type === 'income') {
+          return accumulator + current.value;
+        }
+        return accumulator;
+      },
+      0,
+    );
+
+    const outcome = this.transactions.reduce(
+      (accumulator: number, current: Transaction) => {
+        if (current.type === 'outcome') {
+          return accumulator + current.value;
+        }
+        return accumulator;
+      },
+      0,
+    );
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
